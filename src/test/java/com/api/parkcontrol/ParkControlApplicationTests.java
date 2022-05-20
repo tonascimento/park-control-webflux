ecd942519430ef9e6065c74867deb3f079c92fb8
thiago.oliveira <thiago.oliveira@portoseguro.com.br>
2022-05-20 08:55:20 -0300

    first commit

---
+++ src/test/java/com/api/parkcontrol/ParkControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parkcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
